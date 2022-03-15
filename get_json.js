e265fbc9cf43a7864e0013255b637189a0234c2e
HNH12 <62258989+HNH12@users.noreply.github.com>
2022-03-15 17:51:37 +0300

    Перенос в папку json

--- get_json.js
+++
@@ -1,9 +0,0 @@
-const url = 'https://raw.githubusercontent.com/BibaBoba123/TechnicalSupport_Web/main/test.json';
-
-fetch(url)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(jsonResponse) {
-    console.log(jsonResponse);
-  });
