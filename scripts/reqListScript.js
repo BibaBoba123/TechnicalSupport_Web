6b91c2ad5d52fb815975eadf5c158bc12ad8eb01
anisimovalinaa <62428300+anisimovalinaa@users.noreply.github.com>
2022-03-23 14:32:33 +0300

    feat: список заявок и стили
    создала список заявок из json и поменяла стили

--- scripts/reqListScript.js
+++ scripts/reqListScript.js
@@ -1,27 +1,69 @@
-const nameJsonFile = 'categories.json';
-const url = `https://raw.githubusercontent.com/BibaBoba123/TechnicalSupport_Web/main/json/${nameJsonFile}`;
-
 function createCategoriesItem(nameCategory) {
-    const card = document.createElement('div');
-    card.innerHTML = nameCategory;
-    card.classList = 'menu-categories__item';
+  const card = document.createElement('div');
+  card.innerHTML = nameCategory;
+  card.classList = 'menu-categories__item';
 
-    return card;
+  return card;
 }
 
 function createMenuCategories(categories) {
-    console.log(categories);
-    const menu = document.querySelector('.menu-categories');
-    for (let c in categories) {
-        const card = createCategoriesItem(categories[c]['name']);
-        menu.append(card);
-    }
+  const menu = document.querySelector('.menu-categories');
+  for (let c in categories) {
+      menu.append(createCategoriesItem(categories[c]['name']));
+  }
 }
 
-fetch(url)
+function createReqListItem(data) {
+  const element = document.createElement('div');
+  element.innerHTML = `<strong>${data['theme']}</strong>`;
+  element.classList = 'res-list__item';
+
+  const statusEl = document.createElement('div');
+  statusEl.innerHTML = data['status'];
+  statusEl.classList = 'res-list__item__status';
+  element.append(statusEl);
+
+  const executorEl = document.createElement('div');
+  executorEl.innerHTML = 'Исполнитель: ' + data['executor'];
+  executorEl.classList = 'res-list__item__executor';
+  element.append(executorEl);
+
+  return element;
+}
+
+function createReqList(reqList) {
+  const recListDiv = document.querySelector('.req-list');
+  const reqListItems = [];
+  for (let r in reqList) {
+    const itemObj = {};
+    itemObj['category'] = reqList[r]['category'];
+    itemObj['id'] = reqList[r]['id'];
+
+    const element = createReqListItem(reqList[r]);
+    recListDiv.append(element);
+    
+    itemObj['block'] = element;
+
+    reqListItems.push(itemObj);
+  }
+}
+
+const urlCategories = 'https://raw.githubusercontent.com/BibaBoba123/TechnicalSupport_Web/main/json/categories.json';
+const urlApplications = 'https://raw.githubusercontent.com/BibaBoba123/TechnicalSupport_Web/main/json/applications.json';
+
+fetch(urlCategories)
   .then(function(response) {
     return response.json();
   })
   .then(function(jsonResponse) {
     createMenuCategories(jsonResponse['categories']);
+  });
+
+
+fetch(urlApplications)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(jsonResponse) {
+    createReqList(jsonResponse['applications']);
   });
